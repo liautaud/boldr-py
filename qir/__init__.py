c72599613bf7676d53cdec1d4c0cb511281da7c0
Romain Liautaud <romain.liautaud@lri.fr>
2017-06-14 22:30:34 +0200

    Fixed Thrift declaration and added Protobuf declaration.

--- qir/__init__.py
+++ qir/__init__.py
@@ -1,6 +1,6 @@
 from .values import Null, Number, Double, String, Boolean
 from .operators import Scan, Select, Project, Sort, Limit, Group, Join
 from .functions import Identifier, Lambda, Application, Conditional
-from .lists import ListConstr, ListNil, ListCons, ListDestr
-from .tuples import TupleConstr, TupleNil, TupleCons, TupleDestr
-from .specials import Local, Builtin, Bytecode, Reference
+from .lists import ListNil, ListCons, ListDestr
+from .tuples import TupleNil, TupleCons, TupleDestr
+from .specials import Builtin, Reference
