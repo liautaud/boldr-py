12acf08ad6f3084e07a2d97d5b11e9f2b9ac6dd1
Romain Liautaud <romain.liautaud@lri.fr>
2017-06-26 18:45:23 +0200

    Almost finished implementation of the Decompiler.

--- qir/__init__.py
+++ qir/__init__.py
@@ -1,8 +1,9 @@
 from .values import Null, Number, Double, String, Boolean
 from .operators import Scan, Filter, Project, Sort, Limit, Group, Join
+from .algebra import Div, Minus, Mod, Plus, Star, Power, And, Not, Or, Equal, LowerOrEqual, LowerThan
 from .functions import Identifier, Lambda, Application, Conditional
 from .lists import ListNil, ListCons, ListDestr
 from .tuples import TupleNil, TupleCons, TupleDestr
 from .specials import Builtin, Table
-from .utils import serialize, unserialize
+from .utils import serialize, unserialize, encode, decode
 from .magic import local, batch
--- qir/decompile.py
+++ qir/decompile.py
@@ -1,3 +1,5 @@
+from . import *
+
 import dis
 import graphviz
 
@@ -10,12 +12,21 @@ JUMP_OPNAMES = [
     'JUMP_ABSOLUTE',
     'CONTINUE_LOOP']
 
-BRANCH_OPNAMES = [
+BRANCH_POP_OPNAMES = [
     'POP_JUMP_IF_TRUE',
-    'POP_JUMP_IF_FALSE',
+    'POP_JUMP_IF_FALSE']
+
+BRANCH_MAY_POP_OPNAMES = [
     'JUMP_IF_TRUE_OR_POP',
-    'JUMP_IF_FALSE_OR_POP',
-    'FOR_ITER']
+    'JUMP_IF_FALSE_OR_POP']
+
+BRANCH_OPNAMES = \
+    BRANCH_POP_OPNAMES + \
+    BRANCH_MAY_POP_OPNAMES
+
+
+class PredecessorStacksError(Exception):
+    pass
 
 
 class Decompiler():
@@ -44,12 +55,12 @@ class Decompiler():
         Separate the instructions into blocks and build a control flow graph.
         """
 
-        # The offset before which all next instructions should be added to the
-        # current block as is, no matter its type.
+        # The offset before which all instructions should be added to the
+        # current block as is, no matter their type.
         ignore_until = False
 
-        # Whether to start a new block on the next instruction, even if it is
-        # not a jump target.
+        # Whether to start a new block on the next instruction, even if it
+        # is not a jump target.
         force_new = True
 
         for instruction in instructions:
@@ -84,14 +95,50 @@ class Decompiler():
         for block in self.blocks:
             block.close()
 
+        # Finally, we compute a reverse map of the direct predecessors of
+        # every block so that execute_blocks can run in linear time.
+        for block in self.blocks:
+            for (successor, edge_type) in block.successors:
+                successor.predecessors.append((block, edge_type))
+
     def sort_blocks(self):
-        pass
+        """
+        Compute a topological ordering of the control flow graph.
+
+        This is possible as we have removed cycles from the graph by hiding
+        them into LoopBlocks, and so the graph is a DAG.
+        """
+        marked = [False] * len(self.blocks)
+        ordering = []
+
+        def visit(block):
+            marked[block.index] = True
+
+            for (next, _) in block.successors:
+                if not marked[next.index]:
+                    visit(next)
+
+            ordering.append(block.index)
+
+        for block in self.blocks:
+            if not marked[block.index]:
+                visit(block)
+
+        self.ordering = reversed(ordering)
 
     def execute_blocks(self):
-        pass
+        """
+        Partially execute each block in topological ordering.
+        """
+        for index in self.ordering:
+            self.blocks[index].execute()
 
     def express_blocks(self):
-        pass
+        """
+        Turn each block into a QIR expression in reversed topological ordering.
+        """
+        for index in reversed(self.ordering):
+            self.blocks[index] = ExpressedBlock(self.blocks[index].express())
 
 
 class Block():
@@ -106,6 +153,10 @@ class Block():
 
         # The block to which we should jump once we reach the end of this one.
         self.next = None
+
+        # The blocks from which we could come from.
+        # This reverse map will be filled automatically by build_graph.
+        self.predecessors = []
 
         # Whether the block has already reached a RETURN_VALUE instruction.
         self.reached_return = False
@@ -141,10 +192,184 @@ class Block():
         if self.reached_return:
             self.next = None
 
+    def execute(self):
+        """
+        Ensure that all the block's direct predecessors share the same final
+        stack state, and if so make it the initial state of the block's stack.
+        """
+        stacks = []
+
+        for (predecessor, edge_type) in self.predecessors:
+            # Getting the predecessor's final stack state is slightly tricky
+            # when that predecessor is a BranchBlock, as the stack might be
+            # popped right before the jump depending on the precise branching
+            # instruction and the type of edge that links this block to the
+            # predecessor.
+            if (isinstance(predecessor, BranchBlock) and
+                (predecessor.instruction.opname in BRANCH_POP_OPNAMES or
+                 edge_type == NORMAL_FLOW)):
+                stacks.append(predecessor.stack[:-1])
+            else:
+                stacks.append(predecessor.stack[:])
+
+        if len(stacks) < 1:
+            self.stack = []
+        elif any(stacks[0] != stack for stack in stacks[1:]):
+            raise PredecessorStacksError
+        else:
+            self.stack = stacks[0]
+
+
+BINARY_OPERATIONS = {
+    'BINARY_POWER': Power,
+    'BINARY_MULTIPLY': Star,
+    'BINARY_TRUE_DIVIDE': Div,
+    'BINARY_MODULO': Mod,
+    'BINARY_ADD': Plus,
+    'BINARY_SUBTRACT': Minus}
+
+INPLACE_OPERATIONS = {
+    'INPLACE_POWER': Power,
+    'INPLACE_MULTIPLY': Star,
+    'INPLACE_TRUE_DIVIDE': Div,
+    'INPLACE_MODULO': Mod,
+    'INPLACE_ADD': Plus,
+    'INPLACE_SUBTRACT': Minus}
+
+COMPARE_OPERATIONS = {
+    '==': Equal,
+    '<=': LowerOrEqual,
+    '<': LowerThan}
 
 class LinearBlock(Block):
-    pass
+    def execute(self):
+        super().execute()
 
+        stack = self.stack
+        bindings = []
+
+        for instruction in self.instructions:
+            name = instruction.opname
+
+            # General instructions
+            if name == 'NOP':
+                pass
+            elif name == 'POP_TOP':
+                stack.pop()
+            elif name == 'ROT_TWO':
+                stack[-1], stack[-2] = stack[-2], stack[-1]
+            elif name == 'ROT_THREE':
+                stack[-1], stack[-2], stack[-2] = stack[-3], stack[-1]
+            elif name == 'DUP_TOP':
+                stack.append(stack[-1])
+            elif name == 'DUP_TOP_TWO':
+                stack.append(stack[-2])
+                stack.append(stack[-2])
+
+            # Binary and in-place operations
+            elif name in BINARY_OPERATIONS:
+                right = stack.pop()
+                left = stack.pop()
+                stack.append(BINARY_OPERATIONS[name](left, right))
+            elif name in INPLACE_OPERATIONS:
+                right = stack.pop()
+                left = stack.pop()
+                stack.append(INPLACE_OPERATIONS[name](left, right))
+            elif (name == 'COMPARE_OP' and
+                  instruction.argval in COMPARE_OPERATIONS):
+                right = stack.pop()
+                left = stack.pop()
+                stack.append(COMPARE_OPERATIONS[name](left, right))
+            elif name == 'BINARY_SUBSCR':
+                key = stack.pop()
+                container = stack.pop()
+                stack.append(TupleDestr(container, key))
+            elif name == 'STORE_SUBSCR':
+                key = stack.pop()
+                container = stack.pop()
+                value = stack.pop()
+                stack.append(TupleCons(key, value, container))
+            elif name == 'DELETE_SUBSCR':
+                container = stack.pop()
+                value = stack.pop()
+                stack.append(TupleCons(key, Null, container))
+
+            # Miscellaneous opcodes
+            elif name == 'RETURN_VALUE':
+                self.returns = True
+            elif name == 'LOAD_CONST':
+                stack.append(encode(instruction.argval))
+            elif (name == 'LOAD_NAME' or
+                  name == 'LOAD_GLOBAL' or
+                  name == 'LOAD_FAST'):
+                stack.append(Identifier(instruction.argval))
+            elif name == 'LOAD_ATTR':
+                container = stack.pop()
+                stack.append(TupleDestr(container, String(instruction.argval)))
+            elif (name == 'STORE_NAME' or
+                  name == 'STORE_FAST'):
+                value = stack.pop()
+                bindings.append((instruction.argval, value))
+            elif name == 'STORE_GLOBAL':
+                raise NotImplementedError
+            elif (name == 'DELETE_NAME' or
+                  name == 'DELETE_FAST'):
+                bindings.append((instruction.argval, Null))
+            elif name == 'DELETE_GLOBAL':
+                raise NotImplementedError
+
+            elif (name == 'BUILD_TUPLE' or
+                  name == 'BUILD_LIST' or
+                  name == 'BUILD_SET'):
+                pos = (-1) * instruction.argval
+                values = stack[pos:]
+                stack = stack[:pos]
+
+                container = ListNil()
+                for value in values:
+                    container = ListCons(value, container)
+
+                stack.append(container)
+
+            elif name == 'BUILD_MAP':
+                pos = (-1) * instruction.argval
+                values = stack[pos:]
+                stack = stack[:pos]
+
+                container = TupleNil()
+                for key, value in zip(values[0::2], values[1::2]):
+                    container = TupleCons(key, value, container)
+
+                stack.append(container)
+
+            elif name == 'BUILD_STRING':
+                pos = (-1) * instruction.argval
+                values = stack[pos:]
+                stack = stack[:pos]
+
+                string = ''.join(map(lambda x: x.value, values))
+                stack.append(String(string))
+
+            # Other opcodes
+            else:
+                raise NotImplementedError
+
+        self.stack = stack
+        self.bindings = bindings
+
+    def express(self):
+        if self.next is None and self.reached_return:
+            inner = self.stack[-1]
+        elif self.next is None:
+            inner = Null
+        else:
+            inner = self.next.expression
+
+        # Wrap the expression inside all the variable bindings.
+        for (name, value) in reversed(bindings):
+            inner = Application(Lambda(Identifier(name), wrapped), value)
+
+        return inner
 
 class JumpBlock(Block):
     def __init__(self, context, instruction):
@@ -159,6 +384,9 @@ class JumpBlock(Block):
         # was created between this block and the one that follows it with a
         # new edge between this block and the one it should jump to.
         self.next = self.context.block_mapping[self.instruction.argval]
+
+    def express(self):
+        return self.next.expression
 
 
 class BranchBlock(Block):
@@ -179,6 +407,21 @@ class BranchBlock(Block):
         # to the block to go to if we take the jump.
         self.next_jumped = self.context.block_mapping[self.instruction.argval]
 
+    def express(self):
+        condition = self.stack[-1]
+
+        if self.instruction.opname in \
+                ['POP_JUMP_IF_FALSE', 'JUMP_IF_TRUE_OR_POP']:
+            on_true = self.next.expression
+            on_false = self.next_jumped.expression
+
+        elif self.instruction.opname in \
+                ['POP_JUMP_IF_TRUE', 'JUMP_IF_TRUE_OR_POP']:
+            on_true = self.next_jumped.expression
+            on_false = self.next.expression
+
+        return Conditional(condition, on_true, on_false)
+
 
 class LoopBlock(Block):
     def __init__(self, context, instruction):
@@ -188,7 +431,8 @@ class LoopBlock(Block):
 
 
 class ExpressedBlock(Block):
-    pass
+    def __init__(self, expression):
+        self.expression = expression
 
 
 def decompile(code):
@@ -208,6 +452,8 @@ def preview(code):
 
     decompiler = Decompiler()
     decompiler.build_graph(dis.get_instructions(code))
+    decompiler.sort_blocks()
+    decompiler.execute_blocks()
 
     for block in decompiler.blocks:
         name = block.__class__.__name__
@@ -219,10 +465,11 @@ def preview(code):
                 map(lambda instr: str(instr.offset), block.instructions))
             label = name + '(' + offsets + ')'
 
-        graph.node(str(block.index), label)
+        graph.node(str(block.index), '[' + str(block.index) + '] ' + label)
 
-        for (next, type) in block.successors:
-            graph.edge(str(block.index), str(next.index), label=TYPES[type])
+        for (next, edge_type) in block.successors:
+            graph.edge(
+                str(block.index), str(next.index), label=TYPES[edge_type])
 
     graph.render('test/cfg.gv', view=True)
 
@@ -261,5 +508,5 @@ def bol(x, y, z):
     return u
 
 
-preview(baz)
-dis.dis(baz)
+preview(bol)
+dis.dis(bol)
